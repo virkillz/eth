7aa7d4d0f0494d6813bc026a8e367ba6d8a94e47
VirKill <virkill@gmail.com>
2022-01-14 14:14:23 +0700

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -27,12 +27,12 @@ defmodule Eth.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps() do
     [
-      {:ethereumex, "~> 0.7.0"},
+      {:ethereumex, "~> 0.9.0"},
       {:ex_rlp, "~> 0.5.3"},
-      {:ex_doc, ">= 0.25.2", only: :dev},
+      {:ex_doc, ">= 0.27.3", only: :dev},
       {:dialyxir, "~> 1.1.0", only: [:dev], runtime: false},
       {:hexate, "~> 0.6.1"},
-      {:ex_keccak, "~> 0.2.0"},
+      {:ex_keccak, "~> 0.3.0"},
       {:mnemonic, "~> 0.3.0"},
       {:poison, "~> 5.0.0", only: :test},
       {:ex_secp256k1, "~> 0.2.1"},
